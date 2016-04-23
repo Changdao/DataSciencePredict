d3cf14128e1e027ed9d969656bce4351f8f63f9b
Changdao <jameslau@263.net>
2016-04-24 01:33:37 +0800

    finish server.R

--- server.R
+++ server.R
@@ -10,14 +10,86 @@ wf2<-readRDS("wf2.rds");
 wf3<-readRDS("wf3.rds");
 wf4<-readRDS("wf4.rds");
 
+#make the input formular
+formatInput <- function(input)
+{
+   input <- iconv(input, "latin1", "ASCII", sub=" ");
+   input <- gsub("[^[:alpha:][:space:][:punct:]]", "", input);
 
+   inputCorpus <- VCorpus(VectorSource(input))
+   inputCorpus <- tm_map(inputCorpus, PlainTextDocument)
+   inputCorpus <- tm_map(inputCorpus, removePunctuation)
+   inputCorpus <- tm_map(inputCorpus, removeNumbers)
+   inputCorpus <- tm_map(inputCorpus, stripWhitespace)
+   input <- as.character(inputCorpus[[1]])
+   input <- gsub("(^[[:space:]]+|[[:space:]]+$)", "", input)
+   result <- ""
+
+   if (nchar(input) > 0) {
+       result<-input
+   } 
+   result;
+}
 
 #predict and return a data frame
 predict <- function(rawArgument)
 {  
+    #format input
+    argument <- formatInput(rawArgument);
+    argument <- unlist(strsplit(argument, split=" "));
+    argumentLen <- length(argument);
     
-    predictTerm <- wf1$term[1];
+    found <- FALSE;
+    predictTerm <- as.character(NULL);
     
+    # 4 grams first
+    if (argumentLen >= 3 & !found)
+    {        
+        argument1 <- paste(argument[(argumentLen-2):argumentLen], collapse=" ");
+        searchStr <- paste("^",argument1, sep = "");
+        wf4SearchDataFrame <- wf4[grep (searchStr, wf4$term), ];        
+        found <- length(wf4SearchDataFrame[, 1]) > 1
+
+        if ( found )
+        {
+            predictTerm <- wf4SearchDataFrame[1,1];                
+        }
+        wf4SearchDataFrame <- NULL;
+    }
+
+    # 3 grams next
+    if (argumentLen >= 2 & !found)
+    {        
+        argument1 <- paste(argument[(argumentLen-1):argumentLen], collapse=" ");
+        searchStr <- paste("^",argument1, sep = "");
+        wf3SearchDataFrame <- wf3[grep (searchStr, wf3$term), ];      
+        found <- length(wf3SearchDataFrame[, 1]) > 1;
+        if ( found )
+        {
+            predictTerm <- wf3SearchDataFrame[1,1];
+        }
+        wf3SearchDataFrame <- NULL;
+    }
+
+    # 2 grams next
+    if (argumentLen >= 1 & !found)
+    {
+        argument1 <- argument[argumentLen];
+        searchStr <- paste("^",argument1, sep = "");
+        wf2SearchDataFrame <- wf2[grep (searchStr, wf2$term), ];        
+        found <- length(wf2SearchDataFrame[, 1]) > 1;
+        if ( found )
+        {
+            predictTerm <- wf2SearchDataFrame[1,1];
+        }
+        wf2SearchDataFrame <- NULL;
+    }
+
+    #:( 1 gram last
+    if (!found & argumentLen > 0)
+    {
+        predictTerm <- wf1$term[1];
+    }
 
     resultTerm <- word(predictTerm, -1);
        
